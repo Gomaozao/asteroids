9e84274f249a8cd41a5af464121881f60bde1abd
Gomaozao <40327219+Gomaozao@users.noreply.github.com>
2024-08-23 12:04:27 -0300

    add 60fps limit

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,1))
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
