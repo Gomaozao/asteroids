2708b1c259510201fdd92781177d92c167a0d160
Gomaozao <40327219+Gomaozao@users.noreply.github.com>
2024-08-25 13:19:15 -0300

    add pygame group classes

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import pygame
 from player import Player
 from constants import *
 
+updatable_group = pygame.sprite.Group()
+drawable_group = pygame.sprite.Group()
+Player.containers = (updatable_group, drawable_group)
+
 def main():
     pygame.init()
     clock = pygame.time.Clock()
@@ -13,12 +17,18 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player1.update(dt)
 
+        for updatable in updatable_group:
+            updatable.update(dt)
+        
         screen.fill((0,0,1))
-        player1.draw(screen)
+
+        for drawable in drawable_group:
+            drawable.draw(screen)
+        
         pygame.display.flip()
 
         dt = clock.tick(60)/1000
+
 if __name__ == "__main__":
     main()
