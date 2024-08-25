58e0594f94d9ab2e70188337e95d66af40f96ac8
Gomaozao <40327219+Gomaozao@users.noreply.github.com>
2024-08-25 12:19:06 -0300

    add player spaceship

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
@@ -6,15 +7,15 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player1 = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
     # Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,1))
+        player1.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
         dt = clock.tick(60)/1000
-
 if __name__ == "__main__":
     main()
